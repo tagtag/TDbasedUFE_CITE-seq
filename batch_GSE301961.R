93c274efa5158f072970048e662bd36542d4228a
Y-h. Taguchi <tag@granular.com>
2026-04-18 19:43:05 +0900

    Add files via upload

---
+++ batch_GSE301961.R
@@ -0,0 +1,10 @@
+source("HOSVD.R")
+x <- readMM("GSE301961_matrix_protein.mtx.gz")
+x1 <- readMM("GSE301961_matrix_rna.mtx.gz")
+fit <- hosvd_rank_trunc(
+  x, x1,
+  r1 = 10, r2 = 10, r3 = 10,
+  block_size = 1000L,
+  eigs_tol = 1e-5
+)
+save(file="fit_GSE301961",fit)
